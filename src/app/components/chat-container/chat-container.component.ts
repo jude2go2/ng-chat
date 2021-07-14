3b01cebcb9bc9af19bc2a4c95ac78db0fb6af1e2
didimango <53489397+didimango@users.noreply.github.com>
2021-07-14 11:50:31 +0300

    added get rrom messages, sent messgaes to correct component, routing and navigation

--- src/app/components/chat-container/chat-container.component.ts
+++ src/app/components/chat-container/chat-container.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IChatRoom } from 'src/app/models';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterEvent,
+} from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { IChatRoom, IMessage } from 'src/app/models';
 import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
@@ -8,12 +15,40 @@ import { ChatService } from 'src/app/services/chat.service';
   templateUrl: './chat-container.component.html',
   styleUrls: ['./chat-container.component.scss'],
 })
-export class ChatContainerComponent implements OnInit {
-  public rooms$: Observable<Array<IChatRoom>>;
+export class ChatContainerComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
 
-  constructor(private chatService: ChatService) {
+  public rooms$: Observable<Array<IChatRoom>>;
+  public messages$: Observable<Array<IMessage>>;
+
+  constructor(
+    private chatService: ChatService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.rooms$ = this.chatService.getRooms();
+
+    const roomId: string = activatedRoute.snapshot.url[1].path;
+
+    this.messages$ = this.chatService.getRoomMessages(roomId);
+    console.log('room id', roomId);
+
+    this.subscription.add(
+      router.events
+        .pipe(filter((data) => data instanceof NavigationEnd))
+        .subscribe((data) => {
+          const routeEvent: RouterEvent = <RouterEvent>data;
+          const urlArr = routeEvent.url.split('/');
+          if (urlArr.length > 2) {
+            this.messages$ = this.chatService.getRoomMessages(urlArr[2]);
+          }
+        })
+    );
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
